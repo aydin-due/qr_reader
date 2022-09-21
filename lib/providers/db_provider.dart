2cb1437cf8617e159e2c554d781aa7bb0b5fcc9b
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-21 12:36:12 -0700

    create db & table

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -12,7 +14,25 @@ class DBProvider {
     return _database;
   }
 
-  Future<Database?> initDB() async {
-    return _database;
+  Future<Database> initDB() async {
+    // path de donde almacenar la base de datos
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = documentsDirectory.path + 'ScansDB.db';
+    print(path);
+
+    // crear base de datos
+    return await openDatabase(
+      path,
+      version: 1, // incrementar cuando se quiere volver a ejecutar la creación
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE Scans (
+            id INTEGER PRIMARY KEY,
+            tipo TEXT,
+            valor TEXT
+          )''');
+      },
+    );
   }
 }
