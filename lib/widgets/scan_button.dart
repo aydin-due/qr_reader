be0dfa4b9dd8a80bb45863de98c2168a98a1d68e
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-21 09:32:28 -0700

    read qr code

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      elevation: 0,
-      child: const Icon(Icons.filter_center_focus),
-      onPressed: () {}
-    );
+        elevation: 0,
+        child: const Icon(Icons.filter_center_focus),
+        onPressed: () async {
+          // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3d8bef', 'Cancelar', false, ScanMode.QR);
+          final barcodeScanRes = 'https://fernando-herrera.com';
+          print(barcodeScanRes);
+        });
   }
 }
