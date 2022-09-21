b0dd1f35b61c8f23853f3d415854c12142bf7389
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-21 12:36:53 -0700

    call db provider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/screens/addresses_screen.dart';
 import 'package:qr_reader/screens/map_screen.dart';
@@ -33,7 +34,8 @@ class _HomeScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final uiProvider = Provider.of<UiProvider>(context);
     final currentIndex = uiProvider.selectedMenuOpt;
-    
+    DBProvider.db.database;
+
     switch (currentIndex) {
       case 0:
         return const MapScreen();
