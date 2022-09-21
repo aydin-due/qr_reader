7d6d8740a287c62192383039c56c42656f8b6f0d
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-21 09:38:38 -0700

    db provider

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,18 +1,18 @@
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
-  static Database _database;
+  static Database? _database;
   static final DBProvider db = DBProvider._(); // ._ constructor privado
 
   DBProvider._(); // al hacer un build, siempre obtener la misma instancia
 
-  Future<Database> get database async{
+  Future<Database?> get database async {
     if (_database != null) return _database;
     _database = await initDB();
     return _database;
   }
 
-  Future<Database> initDB() async {
-
+  Future<Database?> initDB() async {
+    return _database;
   }
 }
