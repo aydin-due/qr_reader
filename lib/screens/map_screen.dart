7e8003321e1398f130a365c1de93cddc484cf4e8
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-11 13:12:43 -0700

    show google map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,13 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:qr_reader/providers/db_provider.dart';
+import 'dart:async';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
    
   const MapScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
   @override
   Widget build(BuildContext context) {
+    const CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
 
@@ -15,8 +27,12 @@ class MapScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
-      body: Center(
-         child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
